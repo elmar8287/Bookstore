df49d45458b9f1ed7791f7f117b1c2a610b58518
elmar8287 <L-mar@inbox.ru>
2021-09-29 00:21:48 +0400

    Add navi.css file and start build page as required

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,24 +5,29 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import './navi.css';
 
-export default function App() {
+export default function Navi() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+      <div className="main">
+        <header>
+          <nav>
+            <h1 className="appTitle">Bookstore CMS</h1>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
