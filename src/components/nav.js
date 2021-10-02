8699d207d5321125c8fef0a46da3531e531f5b37
elmar8287 <L-mar@inbox.ru>
2021-10-03 01:27:11 +0400

    Fix linter issues

--- src/components/nav.js
+++ src/components/nav.js
@@ -36,10 +36,10 @@ const Navi = () => {
           className="bi bi-person-fill"
           viewBox="0 0 16 16"
         >
-          <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
         </svg>
-      </span>
-      </ul>      
+        </span>
+      </ul>
     </nav>
   );
 };
