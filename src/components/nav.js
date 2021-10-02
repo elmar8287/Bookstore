eaaa09fee9909b18b9a40da27008c2c5b5ecb7da
elmar8287 <L-mar@inbox.ru>
2021-10-03 01:47:28 +0400

    Fix linter issues

--- src/components/nav.js
+++ src/components/nav.js
@@ -19,7 +19,7 @@ const Navi = () => {
   return (
     <nav className="Navi">
       <ul className="left-nav">
-        <h1>Bookstore CMS</h1>
+        <h1>BookStore CMS</h1>
         {links.map((link) => (
           <li className="left-nav-li" key={link.id}>
             <NavLink to={link.path} className="left-nav-a" activeClassName="active-link" exact>
@@ -27,18 +27,18 @@ const Navi = () => {
             </NavLink>
           </li>
         ))}
-      <span className="right-nav">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          className="bi bi-person-fill"
-          viewBox="0 0 16 16"
-        >
-          <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
-        </svg>
-      </span>
+        <span className="right-nav">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-person-fill"
+            viewBox="0 0 16 16"
+          >
+            <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+          </svg>
+        </span>
       </ul>
     </nav>
   );
