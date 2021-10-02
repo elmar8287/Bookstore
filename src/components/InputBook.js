c4cb7cad0f6206f2e2bdb026a46364b342b4fb08
elmar8287 <L-mar@inbox.ru>
2021-10-02 13:07:55 +0400

    Install npm i uuid

--- src/components/InputBook.js
+++ src/components/InputBook.js
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
-const InputBook = () => (
+function InputBook() {
+  const dispatch = useDispatch();
+  const [inputBook, setInputBook] = useState({
+    title: '',
+    author: '',
+    category: '',
+  });
 
-  <form>
-    <input type="text" placeholder="Book Title" />
-    <input type="text" placeholder="Categories" />
-    <button type="button">Add Book</button>
-  </form>
-);
+  const onChange = (e) => {
+    setInputBook((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const payload = {
+      ...inputBook,
+      id: uuidv4(),
+    };
+    dispatch(addBook(payload));
+  };
+  const { title, author, category } = inputBook;
+  return (
+    <div>
+      <h2>ADD NEW BOOK</h2>
+      <form>
+        <input
+          name="title"
+          type="text"
+          placeholder="Book title"
+          value={title}
+          onChange={onChange}
+        />
+        <input
+          name="author"
+          type="text"
+          placeholder="Book author"
+          value={author}
+          onChange={onChange}
+        />
+        <select name="category" id="categories" value={category} onChange={onChange}>
+          <option value="1st category">1st category</option>
+          <option value="2nd category">2nd category</option>
+          <option value="3d category">3d category</option>
+          <option value="4th category">4th category</option>
+        </select>
+        <button type="button" onClick={handleSubmit}>ADD BOOK</button>
+      </form>
+    </div>
+  );
+}
 
 export default InputBook;
