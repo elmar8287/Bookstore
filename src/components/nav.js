92e3a284bacc8e2348838da565007735374db67d
elmar8287 <L-mar@inbox.ru>
2021-10-03 01:35:56 +0400

    Fix linter issues

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import {
-  NavLink
-} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './navi.css';
 
 const Navi = () => {
-  const links = [{
+  const links = [
+    {
       id: 1,
       path: '/',
       text: 'BOOKS',
@@ -17,45 +16,31 @@ const Navi = () => {
     },
   ];
 
-  return ( <
-    nav className = "Navi" >
-    <
-    ul className = "left-nav" >
-    <
-    h1 > Bookstore CMS < /h1> {
-      links.map((link) => ( <
-        li className = "left-nav-li"
-        key = {
-          link.id
-        } >
-        <
-        NavLink to = {
-          link.path
-        }
-        className = "left-nav-a"
-        activeClassName = "active-link"
-        exact > {
-          link.text
-        } <
-        /NavLink> <
-        /li>
-      ))
-    } <
-    span className = "right-nav" >
-    <
-    svg xmlns = "http://www.w3.org/2000/svg"
-    width = "16"
-    height = "16"
-    fill = "currentColor"
-    className = "bi bi-person-fill"
-    viewBox = "0 0 16 16" >
-    <
-    path d = "M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" / >
-    <
-    /svg> <
-    /span> <
-    /ul> <
-    /nav>
+  return (
+    <nav className="Navi">
+      <ul className="left-nav">
+        <h1>Bookstore CMS</h1>
+        {links.map((link) => (
+          <li className="left-nav-li" key={link.id}>
+            <NavLink to={link.path} className="left-nav-a" activeClassName="active-link" exact>
+              {link.text}
+            </NavLink>
+          </li>
+        ))}
+        <span className="right-nav">
+          <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-person-fill"
+          viewBox="0 0 16 16"
+        >
+          <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+        </svg>
+        </span>
+      </ul>
+    </nav>
   );
 };
 
