44136dd86dca3bc11fc9909b361034b6a1b7ef97
elmar8287 <L-mar@inbox.ru>
2021-10-02 14:14:28 +0400

    Modify BookItem file

--- src/components/BookContainer.js
+++ src/components/BookContainer.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import Navi from './nav';
-import BookList from './BookList';
+import { Route, Switch } from 'react-router-dom';
+import BooksList from './BooksList';
 import InputBook from './InputBook';
+import Categories from '../pages/Categories';
+import Navi from './nav';
 
-const BookContainer = () => (
-  <main>
-    <Navi />
-    <BookList />
-    <div>
-      <BookList />
-      <InputBook />
-    </div>
-  </main>
-);
+function BookContainer() {
+  return (
+    <>
+      <Navi />
+      <Switch>
+        <Route exact path="/">
+          <div>
+            <BooksList />
+            <InputBook />
+          </div>
+        </Route>
+        <Route path="/categories">
+          <Categories />
+        </Route>
+      </Switch>
+    </>
+  );
+}
 
 export default BookContainer;
