6fbec0a85b1ef3056d868dada9e87c6411e0128b
elmar8287 <L-mar@inbox.ru>
2021-10-03 01:38:44 +0400

    Fix linter issues

--- src/components/nav.js
+++ src/components/nav.js
@@ -27,8 +27,8 @@ const Navi = () => {
             </NavLink>
           </li>
         ))}
-        <span className="right-nav">
-          <svg
+      <span className="right-nav">
+        <svg
           xmlns="http://www.w3.org/2000/svg"
           width="16"
           height="16"
@@ -38,7 +38,7 @@ const Navi = () => {
         >
           <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
         </svg>
-        </span>
+      </span>
       </ul>
     </nav>
   );
